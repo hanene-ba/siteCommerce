4e387ee9f7e900a5af94c5fb63b5054aa7e08efe
Hanen_Ben_Ameur <ba.hanene.96gmail.com>
2022-08-21 11:30:17 +0200

    Create-Web-site-for-learning

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Tunisian Crafts</a>
+      </header>
+      <div class="scrolling">
+        <h2>Apprendre l'automatisation des test By Faten</h2>
+      </div>
+      <br />
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
